07967d83bd56f8b93e997def5df19b3c3e6fa571
DesktopKit Dev <desktopkit@example.com>
2026-03-25 20:27:17 +0900

    Phase 1: 共通基盤の構築

---
+++ Diffchecker/MainForm.cs
@@ -0,0 +1,18 @@
+using DesktopKit.Common;
+
+namespace DesktopKit.Diffchecker
+{
+    /// <summary>
+    /// Diffchecker（ファイル差分レポーター）のメインフォーム。
+    /// </summary>
+    public class MainForm : BaseForm
+    {
+        /// <summary>
+        /// MainFormのコンストラクタ。
+        /// </summary>
+        public MainForm()
+        {
+            ComponentName = "Diffchecker";
+        }
+    }
+}
